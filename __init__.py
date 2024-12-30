b14755c4f318f10350c350f635b31a6e275736df
jordan wakelam <jwakelams@gmail.com>
2024-12-31 00:39:23 +0100

    ajout de la page et la route contact

--- __init__.py
+++ __init__.py
@@ -7,11 +7,15 @@ app = Flask(__name__)
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return "<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./flask/exercices/'>Ici</a></p>"
+    return "<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./flask/exercices/'>Ici</a></p><p><a href='./flask/contact/'>Contact</a></p>"
 
 @app.route('/exercices/')
 def exercices():
     return render_template('exercices.html')
+
+@app.route('/contact/')
+def MaPremiereAPI():
+    return render_template('contact.html')
                                                                                                                
 if __name__ == "__main__":
   app.run(debug=True)
