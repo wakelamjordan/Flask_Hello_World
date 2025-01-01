f1dd99e67a9faeddb9349b41eab4766b3d5e9527
jordan wakelam <jwakelams@gmail.com>
2025-01-01 15:41:56 +0100

    normalement tailwind est opérationnel et j'ai commencé à templaté la base avec navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/*.{html,js}"],
+  content: ["./templates/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark"], // Choix des thèmes
+  },
 };
+
+// npx tailwindcss -i ./static/css/input.css -o ./static/css/output.css --minify
+
+// npx tailwindcss -i ./static/css/input.css -o ./dist/output.css --watch
+
+
