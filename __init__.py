dc3e88dd96ed11e3dd6161bf575d6973a681f284
jordan wakelam <jwakelams@gmail.com>
2025-01-02 01:38:37 +0100

    réalisation de toutes les routes api et doc

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, url_for
 from flask import render_template
-from flask import json, jsonify
+from flask import json, jsonify, request
 import sqlite3
 
 app = Flask(__name__)
@@ -17,22 +17,65 @@ def inject_prefix():
 @app.route('/')
 def doc():
     doc_data = {
-        "title": "ddDocumentation de l'API",
-        "endpoints": [
-            # {
-            #     "url": url_for('MaPremiereAPI', _external=True),
-            #     "description": "Renvoie un message de bienvenue"
-            # },
-            {
-                "url": url_for('carre', val_user=5, _external=True),
-                "description": "Calcule le carré d'un nombre"
-            },
-            {
-                "url": url_for('somme', valeur1=5, valeur2=10, _external=True),
-                "description": "Calcule la somme de deux nombres"
+    "title": "Documentation de l'API",
+    "endpoints": [
+        {
+        "url": "/",
+        "method": "GET",
+        "description": "Affiche la documentation de l'API en format JSON."
+        },
+        {
+        "url": "/exercices/",
+        "method": "GET",
+        "description": "Affiche la page HTML des exercices (template Flask)."
+        },
+        {
+        "url": "/calcul_carre/<val_user>",
+        "method": "GET",
+        "parameters": {
+            "val_user": {
+            "type": "int",
+            "description": "Nombre entier pour lequel on veut calculer le carré."
             }
-        ]
+        },
+        "description": "Calcule le carré d'un nombre entier fourni dans l'URL."
+        },
+        {
+        "url": "/calcul_somme/",
+        "method": "GET",
+        "parameters": {
+            "query_params": {
+            "type": "list",
+            "description": "Liste de nombres entiers passés en paramètres GET pour calculer leur somme."
+            }
+        },
+        "description": "Calcule la somme des nombres passés en paramètres GET."
+        },
+        {
+        "url": "/impaire/<valeur>",
+        "method": "GET",
+        "parameters": {
+            "valeur": {
+            "type": "int",
+            "description": "Nombre entier à vérifier s'il est pair ou impair."
+            }
+        },
+        "description": "Détermine si un nombre entier est pair ou impair."
+        },
+        {
+        "url": "/valeur_max/",
+        "method": "GET",
+        "parameters": {
+            "query_params": {
+            "type": "list",
+            "description": "Liste de nombres entiers passés en paramètres GET pour déterminer le maximum."
+            }
+        },
+        "description": "Renvoie la valeur maximale parmi les nombres passés en paramètres GET."
+        }
+    ]
     }
+
     return jsonify(doc_data)
 
 @app.route('/exercices/')
@@ -51,18 +94,90 @@ def carre(val_user):
     }
     return jsonify(data_carre)
 
-@app.route('/calcul_somme/<int:valeur1>/<int:valeur2>')
-def somme(valeur1, valeur2):
+@app.route('/calcul_somme/', methods=['GET'])
+def somme():
+
+    params = request.args.to_dict()
+    
+    result = 0
+    
+    valeurs = ""
+    
+    if params == {}: 
+        return jsonify({"error": "Aucun paramètre n'a été fourni"}), 400
+
+    for value in params.values():
+        if not value.isdigit() or value == "":
+            return jsonify({"error": "Tous les paramètres doivent être des nombres entiers"}), 400
+
+        result += int(value)
+        
+        if valeurs == "":
+            valeurs += value
+        else:
+            valeurs += "+" + value
+
+    # Construire la réponse
     data_somme = {
-        "valeur1": valeur1,
-        "valeur2": valeur2,
-        "somme": valeur1 + valeur2
+        "valeurs": valeurs,
+        "somme": result
     }
     return jsonify(data_somme)
+    # return jsonify(data_somme)
+# @app.route('/calcul_somme/<int:valeur1>/<int:valeur2>')
+# def somme(valeur1, valeur2):
+#     data_somme = {
+#         "valeur1": valeur1,
+#         "valeur2": valeur2,
+#         "somme": valeur1 + valeur2
+#     }
+#     return jsonify(data_somme)
 
-# @app.route('/impaire/<int:valeur>')
-# def impaire(valeur):
+@app.route('/impaire/<int:valeur>')
+def impaire(valeur):
+    if valeur % 2 == 0:
+        response = "paire"
+    else:
+        response = "impaire"
     
+    data_impaire = {
+        "valeur": valeur,
+        "reponse": response
+    }
+    
+    return jsonify(data_impaire)
+    
+    
+@app.route('/valeur_max/', methods=['GET'])
+def valeur_max():
+    params = request.args.to_dict()
+    
+    best_value = 0
+    
+    valeurs = ""
+    
+    if params == {}: 
+        return jsonify({"error": "Aucun paramètre n'a été fourni"}), 400
+    
+    for value in params.values():
+        if not value.isdigit() or value == "":
+            return jsonify({"error": "Tous les paramètres doivent être des nombres entiers"}), 400
+
+        if int(value) > best_value:
+            best_value = int(value)
+            
+        if valeurs == "":
+            valeurs += value
+        else:
+            valeurs += "," + value
+    
+    data_max = {
+        "valeurs": valeurs,
+        "max": best_value
+    }
+    
+    return jsonify(data_max)
+
 
 if __name__ == "__main__":
   app.run(debug=True)
